b9b838cacecdb3f854981b52a821dbaee2bf62e1
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2017-06-29 23:34:46 +0200

    Run eslint --fix on code (yarn run lint)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,88 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
-import {Terminal, Prompt, Line} from '../components/terminal'
+import { Terminal, Prompt, Line } from '../components/terminal'
 
 import './styles.css'
 
-const Header = () => (
-    <div style={{textAlign: 'center', paddingTop: '1em'}}>
-        <h1 style={{
-            fontWeight: 900
-        }}>Hatch.</h1>
+const Header = () =>
+  <div style={{ textAlign: 'center', paddingTop: '1em' }}>
+    <h1
+      style={{
+        fontWeight: 900,
+      }}
+    >
+      Hatch.
+    </h1>
 
-        <h2 style={{
-            fontWeight: 'normal',
-            fontSize: '3rem',
-            lineHeight: '3.2rem',
-            paddingBottom: '1rem'
-        }}>
-            <b>Easy</b> deployment of static websites to Amazon Web Services
-        </h2>
+    <h2
+      style={{
+        fontWeight: 'normal',
+        fontSize: '3rem',
+        lineHeight: '3.2rem',
+        paddingBottom: '1rem',
+      }}
+    >
+      <b>Easy</b> deployment of static websites to Amazon Web Services
+    </h2>
 
-        <p style={{padding: '1.5em 0'}}>
-        Simple command line interface for managing your static websites on
-        AWS — Hatch takes cares of creating S3 buckets, configuring your
-        custom domains, and setting up CloudFront — All from the comforts of
-        your command line.
-        </p>
+    <p style={{ padding: '1.5em 0' }}>
+      Simple command line interface for managing your static websites on AWS — Hatch takes
+      cares of creating S3 buckets, configuring your custom domains, and setting up
+      CloudFront — All from the comforts of your command line.
+    </p>
+  </div>
 
-    </div>
-)
+const Body = () =>
+  <div
+    style={{
+      marginTop: '2em',
+    }}
+  >
+    <p>Hatch automates the deployment of static websites to Amazon Web Services</p>
+  </div>
 
-const Body = () => (
-    <div style={{
-        marginTop: '2em'
-    }}>
-        <p>Hatch automates the deployment of static websites to Amazon Web Services</p>
-
-    </div>
-)
-
-const Footer = () => (
-    <div style={{
-        marginTop: '2rem',
-        fontWeight: '300',
-        fontSize: '0.8rem',
-        textAlign: 'center'
-    }}>
-        <p>
-            Made with ❤️ by
-            <a href="https://twitter.com/mads_hartmann" target="_blank"> Mads Hartmann </a> &
-            <a href="https://twitter.com/kenneth_skovhus" target="_blank"> Kenneth Skovhus</a>
-        </p>
-    </div>
-)
+const Footer = () =>
+  <div
+    style={{
+      marginTop: '2rem',
+      fontWeight: '300',
+      fontSize: '0.8rem',
+      textAlign: 'center',
+    }}
+  >
+    <p>
+      Made with ❤️ by
+      <a href="https://twitter.com/mads_hartmann" target="_blank">
+        {' '}Mads Hartmann{' '}
+      </a>{' '}
+      &
+      <a href="https://twitter.com/kenneth_skovhus" target="_blank">
+        {' '}Kenneth Skovhus
+      </a>
+    </p>
+  </div>
 
 export default () =>
-    <div>
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>Hatch</title>
-            <link href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:300,400,700,900" rel="stylesheet" />
-        </Helmet>
-        <div className="container">
-            <Header />
-            <Terminal>
-                <Prompt>brew tap hatch-sh/hatch</Prompt>
-                <Prompt>brew install hatch</Prompt>
-                <Prompt>hatch deploy</Prompt>
-                <Line>Deploying site</Line>
-                <Line>Site deployed to http://abc.foobar.com</Line>
-            </Terminal>
-            <Body />
-            <Footer />
-        </div>
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Hatch</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:300,400,700,900"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <div className="container">
+      <Header />
+      <Terminal>
+        <Prompt>brew tap hatch-sh/hatch</Prompt>
+        <Prompt>brew install hatch</Prompt>
+        <Prompt>hatch deploy</Prompt>
+        <Line>Deploying site</Line>
+        <Line>Site deployed to http://abc.foobar.com</Line>
+      </Terminal>
+      <Body />
+      <Footer />
     </div>
+  </div>
