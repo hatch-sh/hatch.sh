cab6d3c8efc6bf4808d80b6301711134646beffb
Mads Hartmann <mads379@gmail.com>
2017-07-11 22:27:10 +0200

    Write TOC. Add icon

--- src/pages/getting-started.js
+++ src/pages/getting-started.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import marked from 'marked'
 
+import logo from '../../static/images/hatch-200x240.png'
+
 const markup = marked(`
 
 Hatch makes it easy to manage your static websites on AWS — Hatch takes cares
 of creating S3 buckets, configuring your custom domains in Route53, and setting
 up CloudFront — All from the comforts of your command line.
+
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Deploying](#deploying)
+- [Relevant AWS pricing](#relevant-aws-pricing)
+- [How it works](#how-it-works)
 
 ## Installation
 
@@ -15,11 +23,13 @@ On macOS you can install Hatch using [Homebrew][homebrew]:
 \`\`\`sh
 brew tap hatch-sh/hatch
 brew install hatch
+hatch --version
 \`\`\`
 
-
-This will install hatch as well as ZSH and Bash completion scripts so you get
-nice helpful tab-completions in your shell.
+This will install hatch as well as Zsh and Bash completion scripts so you get
+nice helpful tab-completions in your shell. For the Bash completions to work
+you need to have the \`bash-completion\` homebrew package installed. For Zsh
+completions to work you need to be using [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh).
 
 On other platforms you can install hatch as a pip package
 
@@ -47,10 +57,14 @@ Alternativly you can use the command line arguments
 hatch --todo
 \`\`\`
 
-## Examples
+## Deploying
 
 This site is deployed using Hatch on [Travis][travis]. The site is generated
 using [Gatsby.js][gatsby]. You can see the code [here][hatch-website].
+
+## Relevant AWS pricing
+
+## How it works
 
 [homebrew]: https://brew.sh/
 [travis]: https://travis-ci.org
